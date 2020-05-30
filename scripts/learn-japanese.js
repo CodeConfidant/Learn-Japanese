ac822c17d90b34578c4f6235fbb5bdbe68357af8
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-05-30 18:32:01 -0400

    Updated markup, styles/scripts.

--- scripts/learn-japanese.js
+++ scripts/learn-japanese.js
@@ -1,10 +1,21 @@
-navchange = () => {
-    var nav_container = document.getElementById("nav-links");
+var nav_container = document.getElementById("nav-links");
+var nav_button = document.getElementById("nav-button");
 
+navchange = () => {
     if (nav_container.style.display === "block") {
-      nav_container.style.display = "none";
+      nav_container.style.display = "none"; 
     } 
     else {
       nav_container.style.display = "block";
     }
+};
+
+navhover_on = () => {
+  nav_button.style.borderColor = "dodgerblue";
+  nav_button.style.color = "dodgerblue";
+};
+
+navhover_off = () => {
+  nav_button.style.borderColor = "whitesmoke";
+  nav_button.style.color = "whitesmoke";
 };
