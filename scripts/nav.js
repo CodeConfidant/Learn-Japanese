67e2cb78e5470dba0b4457d3c992aae99aa9f6f5
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-06-10 21:16:04 -0400

    Updated markup, styles, and scripts.

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,19 +3,19 @@ var nav_button = document.getElementById("nav-button");
 
 navchange = () => {
   if (nav_container.style.display === "block") {
-    nav_container.style.display = "none";
+    nav_container.style.display = "none"
   } 
   else {
-    nav_container.style.display = "block";
+    nav_container.style.display = "block"
   }
 };
 
 navhover_on = () => {
-  nav_button.style.borderColor = "dodgerblue";
-  nav_button.style.color = "dodgerblue";
+  nav_button.style.borderColor = "dodgerblue"
+  nav_button.style.color = "dodgerblue"
 };
 
 navhover_off = () => {
-  nav_button.style.borderColor = "whitesmoke";
-  nav_button.style.color = "whitesmoke";
+  nav_button.style.borderColor = "whitesmoke"
+  nav_button.style.color = "whitesmoke"
 };
---
+++ scripts/search.js
@@ -0,0 +1,4 @@
+submit_handler = () => {
+    var search_value = document.getElementById("search-box").value
+    console.log(search_value)
+};
