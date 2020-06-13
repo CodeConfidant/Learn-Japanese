8dd6b12411d5b84146950190d68384dcf6bfff79
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-06-13 13:37:47 -0400

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -1,47 +1,54 @@
-// Return true if the arg string contrains only alphabetical characters.
-isAlpha = (arg) => {
-    var alphabet = /^[a-z]+$/;
-    if(arg.toLowerCase().match(alphabet)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
-
-// Return true if the arg string contrains only numbers.
-isNumber = (arg) => {
-    if (isNaN(arg) === true) {
-        return false
-    }
-    else {
-        return true
-    }
-};
-
-// Return true if the arg string contrains only alphabetical or numerical characters.
-isAlphaNumeric = (arg) => {
-    var alphabet = /^[a-z]+$/;
-    var char_check = false
-
+// Remove all number characters from String.
+trimNumbers = (arg) => {
     for (var i = 0; i < arg.length; i++){
-        if(arg.charAt(i).toLowerCase().match(alphabet)) {
-            char_check = true
+        if (arg.charAt(i) === "0"){
+            arg = arg.replace("0", "")
         }
-        else if (isNaN(arg.charAt(i)) === false) {
-            char_check = true
+
+        if (arg.charAt(i) === "1"){
+            arg = arg.replace("1", "")
         }
-        else {
-            char_check = false
+        
+        if (arg.charAt(i) === "2"){
+            arg = arg.replace("2", "")
+        }
+
+        if (arg.charAt(i) === "3"){
+            arg = arg.replace("3", "")
+        }
+
+        if (arg.charAt(i) === "4"){
+            arg = arg.replace("4", "")
+        }
+
+        if (arg.charAt(i) === "5"){
+            arg = arg.replace("5", "")
+        }
+
+        if (arg.charAt(i) === "6"){
+            arg = arg.replace("6", "")
+        }
+
+        if (arg.charAt(i) === "7"){
+            arg = arg.replace("7", "")
+        }
+
+        if (arg.charAt(i) === "8"){
+            arg = arg.replace("8", "")
+        }
+
+        if (arg.charAt(i) === "9"){
+            arg = arg.replace("9", "")
         }
     }
 
-    return char_check
+    return arg
 };
 
 // Handle the input & submission.
 submit_handler = () => {
-    var search_value = document.getElementById("search-box").value.toLowerCase()
-    console.log(search_value)
-    document.getElementById("search-box").value = ""
+    var search_value = document.getElementById("search-box").value.toLowerCase().trim();
+    search_value = trimNumbers(search_value);
+    console.log(search_value);
+    document.getElementById("search-box").value = "";
 };
