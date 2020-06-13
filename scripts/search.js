51b5f8df0d6a34189fdc46e3c7ddfa434dccdc5d
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-06-13 14:02:50 -0400

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -1,43 +1,43 @@
 // Remove all number characters from String.
 trimNumbers = (arg) => {
-    for (var i = 0; i < arg.length; i++){
-        if (arg.charAt(i) === "0"){
+    for (var i of arg){
+        if (i === "0"){
             arg = arg.replace("0", "")
         }
 
-        if (arg.charAt(i) === "1"){
+        if (i === "1"){
             arg = arg.replace("1", "")
         }
         
-        if (arg.charAt(i) === "2"){
+        if (i === "2"){
             arg = arg.replace("2", "")
         }
 
-        if (arg.charAt(i) === "3"){
+        if (i === "3"){
             arg = arg.replace("3", "")
         }
 
-        if (arg.charAt(i) === "4"){
+        if (i === "4"){
             arg = arg.replace("4", "")
         }
 
-        if (arg.charAt(i) === "5"){
+        if (i === "5"){
             arg = arg.replace("5", "")
         }
 
-        if (arg.charAt(i) === "6"){
+        if (i === "6"){
             arg = arg.replace("6", "")
         }
 
-        if (arg.charAt(i) === "7"){
+        if (i === "7"){
             arg = arg.replace("7", "")
         }
 
-        if (arg.charAt(i) === "8"){
+        if (i === "8"){
             arg = arg.replace("8", "")
         }
 
-        if (arg.charAt(i) === "9"){
+        if (i === "9"){
             arg = arg.replace("9", "")
         }
     }
@@ -45,10 +45,16 @@ trimNumbers = (arg) => {
     return arg
 };
 
+trimSpace = (arg) => {
+    arg = arg.replace(/ /g, "")
+    return arg
+};
+
 // Handle the input & submission.
 submit_handler = () => {
-    var search_value = document.getElementById("search-box").value.toLowerCase().trim();
-    search_value = trimNumbers(search_value);
-    console.log(search_value);
-    document.getElementById("search-box").value = "";
+    var search_value = document.getElementById("search-box").value.toLowerCase().trim()
+    search_value = trimNumbers(search_value)
+    search_value = trimSpace(search_value)
+    console.log(search_value)
+    document.getElementById("search-box").value = ""
 };
