5b032a6a6e16ab47ffc29b605e66b5d09ef3b60f
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-06-17 01:20:04 -0400

    Updated markup & scripts.

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,199 @@
+
+// JavaScript object containing Hiragana translations and kana.
+var hiragana = [
+    {translation: "a", kana: "ぁ"},
+    {translation: "e", kana: "ぇ"},
+    {translation: "i", kana: "い"},
+    {translation: "o", kana: "お"},
+    {translation: "u", kana: "う"},
+
+    {translation: "da", kana: "だ"},
+    {translation: "de", kana: "で"},
+    {translation: "do", kana: "ど"},
+    {translation: "du", kana: "づ"},
+    
+    {translation: "ga", kana: "が"},
+    {translation: "ge", kana: "げ"},
+    {translation: "gi", kana: "ぎ"},
+    {translation: "go", kana: "ご"},
+    {translation: "gu", kana: "ぐ"},
+    
+    {translation: "ha", kana: "は"},
+    {translation: "he", kana: "へ"},
+    {translation: "hi", kana: "ひ"},
+    {translation: "ho", kana: "ほ"},
+    {translation: "hu", kana: "ふ"},
+    
+    {translation: "ka", kana: "か"},
+    {translation: "ke", kana: "け"},
+    {translation: "ki", kana: "き"},
+    {translation: "ko", kana: "こ"},
+    {translation: "ku", kana: "く"},
+    
+    {translation: "ma", kana: "ま"},
+    {translation: "me", kana: "め"},
+    {translation: "mi", kana: "み"},
+    {translation: "mo", kana: "も"},
+    {translation: "mu", kana: "む"},
+    
+    {translation: "n", kana: "ん"},
+    {translation: "na", kana: "な"},
+    {translation: "ne", kana: "ね"},
+    {translation: "ni", kana: "に"},
+    {translation: "no", kana: "の"},
+    {translation: "nu", kana: "ぬ"},
+    
+    {translation: "pa", kana: "ぱ"},
+    {translation: "pe", kana: "ぺ"},
+    {translation: "pi", kana: "ぴ"},
+    {translation: "po", kana: "ぽ"},
+    {translation: "pu", kana: "ぷ"},
+
+    {translation: "ra", kana: "ら"},
+    {translation: "re", kana: "れ"},
+    {translation: "ri", kana: "り"},
+    {translation: "ro", kana: "ろ"},
+    {translation: "ru", kana: "る"},
+    
+    {translation: "sa", kana: "さ"},
+    {translation: "se", kana: "せ"},
+    {translation: "si", kana: "し"},
+    {translation: "shi", kana: "し"},
+    {translation: "so", kana: "そ"},
+    {translation: "su", kana: "す"},
+    {translation: "shu", kana: "す"},
+
+    {translation: "ta", kana: "た"},
+    {translation: "te", kana: "て"},
+    {translation: "ti", kana: "ち"},
+    {translation: "chi", kana: "ち"},
+    {translation: "to", kana: "と"},
+    {translation: "tu", kana: "っ"},
+    {translation: "tsu", kana: "っ"},
+
+    {translation: "wa", kana: "わ"},
+    {translation: "wo", kana: "を"},
+
+    {translation: "ya", kana: "や"},
+    {translation: "yo", kana: "よ"},
+    {translation: "yu", kana: "ゆ"},
+
+    {translation: "za", kana: "ざ"},
+    {translation: "ze", kana: "ぜ"},
+    {translation: "zi", kana: "じ"},
+    {translation: "zo", kana: "ぞ"},
+    {translation: "zu", kana: "ず"}
+];
+
+// JavaScript object containing Katakana translations and kana.
+var katakana = [
+    {translation: "a", kana: "ア"},
+    {translation: "e", kana: "エ"},
+    {translation: "i", kana: "イ"},
+    {translation: "o", kana: "オ"},
+    {translation: "u", kana: "ウ"},
+
+    {translation: "da", kana: "ダ"},
+    {translation: "de", kana: "デ"},
+    {translation: "do", kana: "ド"},
+    {translation: "du", kana: "ヅ"},
+    
+    {translation: "ga", kana: "ガ"},
+    {translation: "ge", kana: "ゲ"},
+    {translation: "gi", kana: "ギ"},
+    {translation: "go", kana: "ゴ"},
+    {translation: "gu", kana: "グ"},
+    
+    {translation: "ha", kana: "ハ"},
+    {translation: "he", kana: "ヘ"},
+    {translation: "hi", kana: "ヒ"},
+    {translation: "ho", kana: "ホ"},
+    {translation: "hu", kana: "フ"},
+    
+    {translation: "ka", kana: "カ"},
+    {translation: "ke", kana: "ケ"},
+    {translation: "ki", kana: "キ"},
+    {translation: "ko", kana: "コ"},
+    {translation: "ku", kana: "ク"},
+    
+    {translation: "ma", kana: "マ"},
+    {translation: "me", kana: "メ"},
+    {translation: "mi", kana: "ミ"},
+    {translation: "mo", kana: "モ"},
+    {translation: "mu", kana: "ム"},
+    
+    {translation: "n", kana: "ン"},
+    {translation: "na", kana: "ナ"},
+    {translation: "ne", kana: "ネ"},
+    {translation: "ni", kana: "ニ"},
+    {translation: "no", kana: "ノ"},
+    {translation: "nu", kana: "ヌ"},
+    
+    {translation: "pa", kana: "パ"},
+    {translation: "pe", kana: "ペ"},
+    {translation: "pi", kana: "ピ"},
+    {translation: "po", kana: "ポ"},
+    {translation: "pu", kana: "プ"},
+
+    {translation: "ra", kana: "ラ"},
+    {translation: "re", kana: "レ"},
+    {translation: "ri", kana: "リ"},
+    {translation: "ro", kana: "ロ"},
+    {translation: "ru", kana: "ル"},
+    
+    {translation: "sa", kana: "サ"},
+    {translation: "se", kana: "セ"},
+    {translation: "si", kana: "シ"},
+    {translation: "shi", kana: "シ"},
+    {translation: "so", kana: "ソ"},
+    {translation: "su", kana: "ス"},
+    {translation: "shu", kana: "ス"},
+
+    {translation: "ta", kana: "タ"},
+    {translation: "te", kana: "テ"},
+    {translation: "ti", kana: "チ"},
+    {translation: "chi", kana: "チ"},
+    {translation: "to", kana: "ト"},
+    {translation: "tu", kana: "ツ"},
+    {translation: "tsu", kana: "ツ"},
+
+    {translation: "wa", kana: "ワ"},
+    {translation: "we", kana: "ヱ"},
+    {translation: "wi", kana: "ヰ"},
+    {translation: "wo", kana: "ヲ"},
+
+    {translation: "ya", kana: "ヤ"},
+    {translation: "yo", kana: "ヨ"},
+    {translation: "yu", kana: "ユ"},
+
+    {translation: "za", kana: "ザ"},
+    {translation: "ze", kana: "ゼ"},
+    {translation: "zi", kana: "ジ"},
+    {translation: "zo", kana: "ゾ"},
+    {translation: "zu", kana: "ズ"}
+];
+
+// JavaScript object containing Kanji translations, kana, and pronunciations.
+var kanji = [
+    {translation: "ground", kana:"地", pronunciation:"ji"},
+    {translation: "water", kana:"水", pronunciation:"mizu"},
+    {translation: "wind", kana:"風", pronunciation:"kaze"},
+    {translation: "rain", kana:"雨", pronunciation:"ame"},
+    {translation: "fire", kana:"火", pronunciation:"hi"},
+    {translation: "thunder", kana:"雷", pronunciation:"kaminari"},
+
+    {translation: "lake", kana:"湖", pronunciation:"mizumi"},
+    {translation: "island", kana:"島", pronunciation:"shima"},
+    {translation: "mountain", kana:"山", pronunciation:"yama"}
+];
+
+// JavaScript object containing General translations, kana, and pronunciations.
+var general = [
+    {translation: "hello", kana:"こにちわ", pronunciation:"konichiwa"},
+    {translation: "goodbye", kana:"さよむら", pronunciation:"sayonara"},
+    {translation: "thank-you", kana:"ありがと", pronunciation:"arigato"}
+];
+
 // Remove number characters from argument string and return.
 trimNumbers = (arg) => {
     for (var i of arg){
@@ -198,8 +394,61 @@ reset = () => {
     document.getElementById("search-box").value = ""
 };
 
-// Handle the input & submission.
-submit_handler = () => {
+// Log search match to console.
+search_log = (arg) => {
+    for (var i of hiragana) {
+        if (arg === i.translation) {
+            console.log("Hiragana: { Translation:", i.translation, ", Kana:", i.kana, "}")
+        }   
+        else if (arg === i.kana) {
+            console.log("Hiragana: { Translation:", i.translation, ", Kana:", i.kana, "}")
+        }
+    }
+
+    for (var i of katakana) {
+        if (arg === i.translation) {
+            console.log("Katakana: { Translation:", i.translation, ", Kana:", i.kana, "}")
+        }   
+        else if (arg === i.kana) {
+            console.log("Katakana: { Translation:", i.translation, ", Kana:", i.kana, "}")
+        }
+    }
+
+    for (var i of kanji) {
+        temp_translation = trimAll(i.translation)
+        temp_kana = trimAll(i.kana)
+        temp_pronunciation = trimAll(i.pronunciation)
+
+        if (arg === temp_translation) {
+            console.log("Kanji: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }   
+        else if (arg === temp_kana) {
+            console.log("Kanji: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }
+        else if (arg === temp_pronunciation) {
+            console.log("Kanji: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }
+    }
+
+    for (var i of general) {
+        temp_translation = trimAll(i.translation)
+        temp_kana = trimAll(i.kana)
+        temp_pronunciation = trimAll(i.pronunciation)
+
+        if (arg === temp_translation) {
+            console.log("General: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }   
+        else if (arg === temp_kana) {
+            console.log("General: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }
+        else if (arg === temp_pronunciation) {
+            console.log("General: { Translation:", i.translation, ", Kana:", i.kana, ", Pronunciation:", i.pronunciation, "}")
+        }
+    }
+};
+
+// Handle the search submission.
+search_handler = () => {
     var search_value = trimAll(document.getElementById("search-box").value.toLowerCase())
-    console.log(search_value)
+    search_log(search_value) 
 };
