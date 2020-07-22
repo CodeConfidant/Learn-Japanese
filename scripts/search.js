8fbb70da7a46a2e70b8f99440728c09e1312b625
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-07-21 21:09:22 -0400

    Updated markup & scripts

--- scripts/search.js
+++ scripts/search.js
@@ -229,15 +229,32 @@ var katakana = [
 
 // Object containing Kanji transliterations, kana, and translations.
 var kanji = [
+
+    // Weather/Elements
     {translation: "Ground", kana:"地", transliteration:"Ji"},
     {translation: "Water", kana:"水", transliteration:"Mizu"},
     {translation: "Wind", kana:"風", transliteration:"Kaze"},
     {translation: "Rain", kana:"雨", transliteration:"Ame"},
     {translation: "Fire", kana:"火", transliteration:"Hi"},
     {translation: "Thunder", kana:"雷", transliteration:"Kaminari"},
+
+    // Geography
     {translation: "Lake", kana:"湖", transliteration:"Mizumi"},
     {translation: "Island", kana:"島", transliteration:"Shima"},
-    {translation: "Mountain", kana:"山", transliteration:"Yama"}
+    {translation: "Mountain", kana:"山", transliteration:"Yama"},
+
+    // Bushido Virtues
+    {translation: "Righteousness", kana:"義", transliteration:"Gi"},
+    {translation: "Courage", kana:"勇", transliteration:"Yu"},
+    {translation: "Compassion", kana:"仁", transliteration:"Jin"},
+    {translation: "Respect", kana:"礼", transliteration:"Rei"},
+    {translation: "Honesty", kana:"誠", transliteration:"Makoto"},
+    {translation: "Honor", kana:"名誉", transliteration:"Meiyo"},
+    {translation: "Loyalty", kana:"忠義", transliteration:"Chugi"},
+    {translation: "Self-Control", kana:"自制", transliteration:"Jisei"},
+    {translation: "Filial Piety", kana:"孝", transliteration:"Ko"},
+    {translation: "Wisdom", kana:"智", transliteration:"Chi"},
+    {translation: "Fraternity", kana:"悌", transliteration:"Tei"},
 ];
 
 // Object containing General Hiragana transliterations, kana, and translations.
